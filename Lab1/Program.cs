3e7bf55170bcc848410a7bbc6afedb24ec37c876
Илья <@DESKTOP-VPI29MR>
2024-09-29 12:54:25 +0200

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    Console.WriteLine("Введите число K: ");
+    int K = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число x: ");
+    int x = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число a: ");
+    int a = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число b: ");
+    int b = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число y: ");
+    int y = int.Parse(Console.ReadLine());
+
+    double D = Math.Pow(K, Math.Asin(-x)) - (a * Math.Sqrt(6)) - (Math.Cos(3 * a * b)) / (Math.Sin(a * Math.Asin(x) + Math.Log10(y))) + (Math.Cos(a * Math.Asin(x) + Math.Log10(y)));
+    Console.WriteLine($"Ввывод : D = {D:F2}");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
