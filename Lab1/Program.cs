bf1cf392b6ba4e225b23dd81bd62e34d0b928e15
justikusX <ilya.blagochinny@gmail.com>
2024-11-12 11:22:55 +0200

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,24 +1,35 @@
-﻿try
+using System;
+
+class Program
 {
-    Console.WriteLine("Введите число K: ");
-    int K = int.Parse(Console.ReadLine());
+    static void Main(string[] args)
+    {
+        // Ввод данных
+        Console.Write("Введите накопления (S) в грн: ");
+        decimal S = decimal.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите число x: ");
-    int x = int.Parse(Console.ReadLine());
+        Console.Write("Введите ежемесячную стипендию (A) в грн: ");
+        decimal A = decimal.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите число a: ");
-    int a = int.Parse(Console.ReadLine());
+        Console.Write("Введите ежемесячные расходы на проживание (B) в грн: ");
+        decimal B = decimal.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите число b: ");
-    int b = int.Parse(Console.ReadLine());
+        // Параметры
+        decimal inflationRate = 0.03m; // 3% рост цен
+        int months = 0; // Количество месяцев
 
-    Console.WriteLine("Введите число y: ");
-    int y = int.Parse(Console.ReadLine());
+        // Расчет
+        while (S + A > B)
+        {
+            months++;
+            // Уменьшаем накопления на текущие расходы
+            S = S + A - B;
 
-    double D = Math.Pow(K, Math.Asin(-x)) - (a * Math.Sqrt(6)) - (Math.Cos(3 * a * b)) / (Math.Sin(a * Math.Asin(x) + Math.Log10(y))) + (Math.Cos(a * Math.Asin(x) + Math.Log10(y)));
-    Console.WriteLine($"Ввывод : D = {D:F2}");
-}
-catch
-{
-    Console.WriteLine("Error");
+            // Увеличиваем расходы на 3%
+            B *= (1 + inflationRate);
+        }
+
+        // Вывод результата
+        Console.WriteLine($"Студент сможет прожить {months} месяцев.");
+    }
 }
